4a8b93f126e4480b8d0e79120bc360c3c8d4a3c9
Nikita Vasilev <nekitvas@gmail.com>
2025-09-18 14:46:11 +0300

    fix

--- ydb/core/kqp/host/kqp_statement_rewrite.cpp
+++ ydb/core/kqp/host/kqp_statement_rewrite.cpp
@@ -229,7 +229,7 @@ namespace {
         const TString tmpTableName = TStringBuilder()
             << tableName
             << "_cas_"
-            << TAppData::RandomProvider->GenRand();
+            << TAppData::RandomProvider->GenUuid4().AsUuidString();
 
         const TString createTableName = (TStringBuilder()
                 << CanonizePath(AppData()->TenantName)
